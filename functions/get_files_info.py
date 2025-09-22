e28e0eb161aeea8e81c59745691c6d567d139dca
1ndr <archie.jayawantha@gmail.com>
2025-09-23 09:42:51 +1000

    working through get_files_info.py file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,10 +1,18 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    full_path = os.path.join(working_directory, directory)
-    absolute_path = os.path.abspath(full_path)
+    abs_path_to_dir = os.path.abspath(directory)
+    abs_path_to_work_dir = os.path.abspath(working_directory)
 
+    rel_path_dir = os.path.join(working_directory, directory)
+    abs_path_to_dir_from_work_dir = os.path.abspath(rel_path_dir)
 
+    if abs_path_to_dir != abs_path_to_dir_from_work_dir:
+        raise Exception(f"rror: Cannot list "{directory}" as it is outside the permitted working directory")
+
+    if: 
+
+    
 
 def finding_directory_in_working_directory(working_directory, directory):
     
