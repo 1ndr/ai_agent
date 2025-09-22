76467dcfb3cddac9beff481c74bedea9d47bb7fe
1ndr <archie.jayawantha@gmail.com>
2025-09-22 22:00:56 +1000

    writing get files module

---
+++ functions/get_files_info.py
@@ -0,0 +1,10 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+    absolute_path = os.path.abspath(full_path)
+
+
+
+def finding_directory_in_working_directory(working_directory, directory):
+    
