a290490d73f82dd94a89a596949a0dfe855b5d37
1ndr <archie.jayawantha@gmail.com>
2026-02-26 19:07:57 +1100

    changed the get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,45 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    obspath_w_directory = os.path.abspath(working_directory)
-    obspath_w_directory_and_directory = os.path.join(obspath_w_directory, directory)
+    abs_path_wd = os.path.abspath(working_directory)
+    dir_path = os.path.join(abs_path_wd, directory)
+    target_dir = os.path.normpath(dir_path)
 
-    full_path_to_directory = os.path.join(working_directory, directory)
-    obspath_full_path_to_directory = os.path.abspath(full_path_to_directory)
+    shared_path = os.path.commonpath(abs_path_wd, target_dir)
+    is_valid_path = abs_path_wd == shared_path
 
-    if obspath_w_directory_and_directory != obspath_full_path_to_directory:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory"
-
-    if os.path.isdir(obspath_full_path_to_directory):
-        return f'Error: "{directory}" is not a directory'
+    if not is_valid_path:
+        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        return
     
-    items_list = os.listdir(directory)
+    if not os.path.isdir(directory):
+        print(f'Error: "{directory}" is not a directory")
+        return
+    
+    dir_content_lst = os.listdir(shared_path)
+    return_arr = []
+    for item in dir_content_lst:
+        try:
+            item_file_path = os.path.join(shared_path, item)
+        except:
+            print(f"Error: unable to find file path for {item}")
+            return
 
-    for item in items_list:
-        is_dir = not os.path.isfile(item)
-        file_size = os.path.getsize(item)
-        print f'- {item}: file_size={file_size} bytes, is_dir={is_dir}\n'
+        try:
+            size = os.path.getsize(item_file_path)
+        except:
+            print(f"Error: can't find size for {item}")
+            return
+
+        try:
+            is_dir = os.path.isdir(item_file_path)
+        except:
+            print(f"Error: can't find if {item} is directory or file.")
+            return
+        return_arr.append(f"- {item}: file_size={size} bytes, is_dir={is_dir}")
+
+    return " /n",join(return_arr)
+
+   
+
+
