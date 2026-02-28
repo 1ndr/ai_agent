102627b365ec0093c1c576a1ef16fea9f79d6806
1ndr <archie.jayawantha@gmail.com>
2026-02-28 20:10:44 +1100

    completed get_files_info.py and basic testcases.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,40 +5,36 @@ def get_files_info(working_directory, directory="."):
     dir_path = os.path.join(abs_path_wd, directory)
     target_dir = os.path.normpath(dir_path)
 
-    shared_path = os.path.commonpath(abs_path_wd, target_dir)
+    shared_path = os.path.commonpath([abs_path_wd, target_dir])
     is_valid_path = abs_path_wd == shared_path
 
     if not is_valid_path:
-        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-        return
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    if not os.path.isdir(directory):
-        print(f'Error: "{directory}" is not a directory")
-        return
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
     
-    dir_content_lst = os.listdir(shared_path)
+    dir_content_lst = os.listdir(target_dir)
     return_arr = []
     for item in dir_content_lst:
         try:
-            item_file_path = os.path.join(shared_path, item)
+            item_file_path = os.path.join(target_dir, item)
         except:
-            print(f"Error: unable to find file path for {item}")
-            return
+            return f"Error: unable to find file path for {item}"
 
         try:
             size = os.path.getsize(item_file_path)
         except:
-            print(f"Error: can't find size for {item}")
-            return
+            return f"Error: can't find size for {item}"
 
         try:
             is_dir = os.path.isdir(item_file_path)
         except:
-            print(f"Error: can't find if {item} is directory or file.")
-            return
+            return f"Error: can't find if {item} is directory or file."
+
         return_arr.append(f"- {item}: file_size={size} bytes, is_dir={is_dir}")
 
-    return " /n",join(return_arr)
+    return "\n".join(return_arr)
 
    
 
---
+++ test_get_files_info.py
@@ -0,0 +1,19 @@
+import unittest
+from functions.get_files_info import get_files_info
+
+#Test Case 1
+print("Results for current directory:")
+print(get_files_info('calculator', '.'))
+
+#Test Case 2
+print("Results for \'pkg\' directory:")
+print(get_files_info('calculator', 'pkg'))
+
+#Test Case 3
+print("Results for '/bin' directory:")
+print(get_files_info('calculator', '/bin/'))
+
+#Test Case 4
+print("Results for '../' directory:")
+print(get_files_info('calculator', '../'))
+
