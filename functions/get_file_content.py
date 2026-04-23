296044b4ff1839d52462589a8091dd72e519f2f1
1ndr <archie.jayawantha@gmail.com>
2026-04-23 20:53:29 +1000

    added test cases for get_file_content.py

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -15,8 +15,17 @@ def get_file_content(working_directory, file_path):
         if not os.path.isfile(target_path):
             return f'Error: File not found or is not a regular file: "{file_path}"' 
         
-        with open(target_path) as f:
-            f.read()
+        with open(target_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+
+            if f.read(1) != '':
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            return file_content_string
+
+    except Exception as e:
+        return f'Error: {e}'
+
+
 
 
 
---
+++ test_get_file_content.py
@@ -0,0 +1,22 @@
+import unittest
+from functions.get_file_content import get_file_content
+
+#Test Case 1
+print('Test Case 1')
+print(get_file_content('calculator', 'lorem.txt'))
+
+#Test Case 2
+print('Test Case 2')
+print(get_file_content('calculator', 'main.py'))
+
+#Test Case 3
+print('Test Case 3')
+print(get_file_content('calculator', 'pkg/calculator.py'))
+
+#Test Case 4
+print('Test Case 4')
+print(get_file_content('calculator', '/bin/cat'))
+
+#Test Case 5
+print('Test Case 5')
+print(get_file_content('calculator', 'pkg/does_not_exist.py'))
