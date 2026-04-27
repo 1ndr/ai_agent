9dd478fed757e595e1b9a68266cc7e16ced278e0
1ndr <archie.jayawantha@gmail.com>
2026-04-27 15:23:53 +1000

    fixed issues with main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import os
 import argparse
+import os
 
 from dotenv import load_dotenv
-from prompts import system_prompt
-from call_function import available_functions
 from google import genai
 from google.genai import types
+
+from call_function import available_functions
+from prompts import system_prompt
 
 def main():
     load_dotenv()
@@ -13,7 +14,7 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-    parser = argparse.ArgumentParser(description="Waffle")
+    parser = argparse.ArgumentParser(description="Wafflehaus")
     parser.add_argument("user_prompt", type=str, help="Please insert a prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
@@ -21,28 +22,37 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     client = genai.Client(api_key=api_key)
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents = messages,
         config = types.GenerateContentConfig(
             tools=[available_functions], 
             system_instruction = system_prompt,
-            temperature= 0)
+            temperature= 0
+        )
     )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
    
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
+    if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    functions_call_present = False
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return
+
     for function_call in response.function_calls:
         print(f"Calling function: {function_call.name}({function_call.args})")
         functions_call_present = True
-
-    if functions_call_present == False:
-        print(response.text)
-
 
 if __name__ == "__main__":
     main()
